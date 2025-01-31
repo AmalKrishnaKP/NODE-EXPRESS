daaf150a0e866c65ba3d0275cad84e1d51960baf
AmalKrishnaKP <148940182+AmalKrishnaKP@users.noreply.github.com>
2025-01-31 20:44:48 +0530

    Add files via upload

---
+++ 5_eventLoop/5_eventLoopEG2.js
@@ -0,0 +1,9 @@
+//start operating system process
+console.log("the first");
+
+setTimeout(()=>{
+    console.log("the second");
+},1000)
+
+console.log("the third");
+// completed and exited the operating system process
