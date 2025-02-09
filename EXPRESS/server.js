ae53a14b1f3fd9fa50530a7cf9b3703d4398ee6b
AmalKrishnaKP <amalkrishnakp04@gmail.com>
2025-02-09 15:06:21 +0530

    express added to json and basic http server created

--- EXPRESS/server.js
+++ EXPRESS/server.js
@@ -1,8 +1,43 @@
 const http=require('http')
+const {readFileSync}=require('fs')
+const homePage=readFileSync('./index.html')
 
 const server=http.createServer((req,res)=>{
-    console.log("user hit the server")
-    res.end('jello')
+    // console.log(req)
+    // console.log(req.method)
+    // console.log(req.url)
+    const urls=req.url
+
+    //home page
+    // 200_-successfull req
+    if(urls==="/")
+    {
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write(homePage)
+        res.end()
+
+    }
+    // about page
+    // 200_-successfull req
+
+    else if(urls==="/about")
+        {
+            res.writeHead(200,{'content-type':'text/html'})
+            res.write('<h1>About Page</h1>')
+            res.end()
+    
+        }
+    //404 -error
+    else
+    {
+        res.writeHead(404,{'content-type':'text/html'})
+        res.write('<h1>Page Not Found</h1>')
+        res.end()
+    }
 })
 
-server.listen(5000)
+server.listen(5100)
+
+
+//http response status code
+// MIME type
