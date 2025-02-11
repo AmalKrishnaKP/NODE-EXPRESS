e47864974757f2ab01f24d551bde12ea4c18cf34
AmalKrishnaKP <amalkrishnakp04@gmail.com>
2025-02-11 23:06:10 +0530

    implimentation file

--- EXPRESS/1_server.js
+++ EXPRESS/1_server.js
@@ -1,20 +1,32 @@
-
-
-
 const express=require('express')
-const path=require('path')
-
 const app=express()
+const {products}=require('./data')
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./sample/index.html'))
+    res.send(`<h1>home page</h1><a href='/api/products'>products</a>`)
 })
 
-
-app.all('*',(req,res)=>{
-    res.status(400).send(`<h1>page not found</h1>`)
+app.get('/api/products',(req,res)=>{
+    const newpro=products.map((product)=>{
+        const {id,name,price}=product
+        return {id,name,price}
+    })
+    res.json(newpro)
 })
 
+app.get('/api/products/:id',(req,res)=>{//route parameter
+    // console.log(req.params);
+
+    const {id}=req.params
+    console.log(id)
+    
+    const singlepro=products.find((product)=>product.id ===Number(id))
+    
+    if(!singlepro){
+        return res.status(404).send(`<h1>Product not Found</h1>`)
+    }
+    return res.json(singlepro)
+})
 app.listen(5000,()=>{
-    console.log("server 5000 is listening...")
+    console.log('server is listening in the port number 5000...')
 })
