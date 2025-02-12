f55cb4053905f29a2388280d6cfe4d33d8055599
AmalKrishnaKP <amalkrishnakp04@gmail.com>
2025-02-12 23:13:20 +0530

    testing server(local host)

--- EXPRESS/1_server.js
+++ EXPRESS/1_server.js
@@ -1,32 +1,35 @@
 const express=require('express')
+
 const app=express()
+
 const {products}=require('./data')
+// console.log(products)
 
-app.get('/',(req,res)=>{
-    res.send(`<h1>home page</h1><a href='/api/products'>products</a>`)
-})
+app.get('/api/v1/query',(req,res)=>{
+    // console.log(req.query)
+    const {search,limit}=req.query
+    // console.log(typeof(search))
+    let newProducts=[...products]
 
-app.get('/api/products',(req,res)=>{
-    const newpro=products.map((product)=>{
-        const {id,name,price}=product
-        return {id,name,price}
-    })
-    res.json(newpro)
-})
-
-app.get('/api/products/:id',(req,res)=>{//route parameter
-    // console.log(req.params);
-
-    const {id}=req.params
-    console.log(id)
     
-    const singlepro=products.find((product)=>product.id ===Number(id))
-    
-    if(!singlepro){
-        return res.status(404).send(`<h1>Product not Found</h1>`)
+
+    if(search)
+    {
+        newProducts=newProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+        console.log(newProducts)
     }
-    return res.json(singlepro)
+    if(limit)
+    {
+        newProducts=newProducts.slice(0,Number(limit))
+    }
+    res.json(newProducts)
+    // res.send(`<h1>home  page</h1>`)
+
 })
-app.listen(5000,()=>{
-    console.log('server is listening in the port number 5000...')
+
+app.listen(5010,()=>{
+    console.log("server running in port number 5010 ..");
+    
 })
