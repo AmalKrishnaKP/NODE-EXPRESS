91fbcee175f6c9a294f86a28188c26ed8185de32
AmalKrishnaKP <amalkrishnakp04@gmail.com>
2025-02-12 23:12:37 +0530

    querystring and accessing data according to it

---
+++ EXPRESS/7_queryString.js
@@ -0,0 +1,34 @@
+const express=require('express')
+
+const app=express()
+
+const {products}=require('./data')
+// console.log(products)
+
+app.get('/api/v1/query',(req,res)=>{
+    // console.log(req.query)
+    const {search,limit}=req.query
+    // console.log(typeof(search))
+    let newProducts=[...products]
+
+    
+
+    if(search)
+    {
+        newProducts=newProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit)
+    {
+        newProducts=newProducts.slice(0,Number(limit))
+    }
+    res.json(newProducts)
+    // res.send(`<h1>home  page</h1>`)
+
+})
+
+app.listen(5010,()=>{
+    console.log("server running in port number 5010 ..");
+    
+})
